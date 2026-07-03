58fb624cc6243a2955825130b1675bd923a6fcbf
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-07-03 14:33:05 +0600

    JDBCtemplate-SchemaAndDataFiles

--- src/main/java/com/example/SpringJDBC/repo/StudentRepo.java
+++ src/main/java/com/example/SpringJDBC/repo/StudentRepo.java
@@ -1,6 +1,8 @@
 package com.example.SpringJDBC.repo;
 
 import com.example.SpringJDBC.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,8 +11,22 @@ import java.util.List;
 @Repository
 public class StudentRepo {
 
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public void save(Student student) {
-        System.out.println("StudentRepo save");
+//        System.out.println("StudentRepo save");
+        String sql = "insert into student(rollNo, name, marks)  values(?,?,?)";
+        int rows = jdbcTemplate.update(sql,  student.getRollNo(), student.getName(), student.getMarks());
+        System.out.println(rows);
     }
 
     public List<Student> findAll() {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student (rollNo, name, marks) values (104, 'Pritam', 44);
+insert into student (rollNo, name, marks) values (105, 'Priya', 55);
+insert into student (rollNo, name, marks) values (106, 'Prity', 66);
+insert into student (rollNo, name, marks) values (107, 'Joba', 77);
