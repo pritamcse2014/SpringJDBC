80ee49d244e4ac7463dfc1bdfc06e7966f22b5d2
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-07-03 14:54:21 +0600

    Rowmapper

--- src/main/java/com/example/SpringJDBC/repo/StudentRepo.java
+++ src/main/java/com/example/SpringJDBC/repo/StudentRepo.java
@@ -3,8 +3,11 @@ package com.example.SpringJDBC.repo;
 import com.example.SpringJDBC.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,7 +33,33 @@ public class StudentRepo {
     }
 
     public List<Student> findAll() {
-        List<Student> students = new ArrayList<>();
-        return students;
+//        List<Student> students = new ArrayList<>();
+//        return students;
+        String sql = "select * from student";
+//        1
+//        RowMapper<Student> rowMapper = new RowMapper<Student>() {
+//            @Override
+//            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+//        2
+//        RowMapper<Student> rowMapper = (rs,rowNum) -> {
+//                Student student = new Student();
+//                student.setRollNo(rs.getInt("rollNo"));
+//                student.setName(rs.getString("name"));
+//                student.setMarks(rs.getInt("marks"));
+//                return student;
+////            }
+//        };
+//
+//        return jdbcTemplate.query(sql, rowMapper);
+
+//        3
+        return jdbcTemplate.query(sql, (rs,rowNum) -> {
+            Student student = new Student();
+            student.setRollNo(rs.getInt("rollNo"));
+            student.setName(rs.getString("name"));
+            student.setMarks(rs.getInt("marks"));
+            return student;
+        });
     }
 }
